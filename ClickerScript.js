5c05d7cc9878f5cd5acac7054b162e7b31f93c1d
mcromac-alt <1234556789ro@gmail.com>
2025-06-30 14:48:45 +0300

    Update ClickerScript.js

--- ClickerScript.js
+++ ClickerScript.js
@@ -2,12 +2,22 @@ var money = 0;
 var income = 1;
 const moneytext = document.getElementById("moneytext");
 const block = document.getElementById("Block");	
+var blocks = ["cobblestone","coal","copper","iron","gold"]
     
 function tap() {
     block.style.width = "75%";
   			money = money + income;
     new Audio('sounds/stonekick.ogg').play()
     moneytext.textContent = `${money} Камней`;
-    console.log(money);
+    console.log(`Money: ${money});
     setTimeout(() => { block.style.width = "80%"; }, 70);
 			}
+
+function upgrade() {
+    if (money>=nextlvlmoney) {
+        ++level
+        income = income * level
+        console.log(`Achieved ${blocks[level]} level.`)
+        block.src=`${blocks[level]}.png
+    }
+}
