4d82fdbacb5ae19190678c893a75c9ae28f124ac
mcromac-alt <1234556789ro@gmail.com>
2025-06-30 09:35:33 +0300

    Update ClickerScript.js

--- ClickerScript.js
+++ ClickerScript.js
@@ -1,1 +1,13 @@
-no
+var money = 0;
+			var income = 1;
+      const moneytext = document.getElementById("moneytext");
+    	const block = document.getElementById("Block");	
+    
+			function tap() {
+        block.style.width = "75%";
+  			money = money + income;
+        new Audio('sounds/stonekick.ogg').play()
+        moneytext.textContent = `${money} Камней`;
+        console.log(money);
+        setTimeout(() => { block.style.width = "80%"; }, 70);
+			}
